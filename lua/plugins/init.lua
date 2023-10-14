64649dc7b970ccf7a7c9f505e2fc86180e1789a9
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2023-10-14 11:01:42 +0200

    plugins: add auto-session

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -222,4 +222,15 @@ return {
             })
         end,
     },
+
+    -- Session managment
+    {
+        'rmagatti/auto-session',
+        config = function()
+            require('auto-session').setup {
+                log_level = "error",
+                auto_session_suppress_dirs = { '~/', '/', "~/.config/*" },
+            }
+        end
+    }
 }
