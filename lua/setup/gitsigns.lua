f4f09e94cbe5e8ad23234d9491bc1c043094ea00
Max Isacson <max.isacson@excillum.com>
2023-11-16 09:05:44 +0100

    gitsigns: add more keybindings

--- lua/setup/gitsigns.lua
+++ lua/setup/gitsigns.lua
@@ -1,12 +1,19 @@
 local gitsigns = require('gitsigns')
 
+local map = function(mode, lhs, rhs, bufnr, desc)
+    vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, silent = true, desc = '[GitSigns] ' .. desc })
+end
+
 local on_attach = function(bufnr)
+    map('n', '<leader>gl', function() gitsigns.blame_line({ full = true }) end, bufnr, "Blame current line")
+    map('n', '<leader>gb', gitsigns.toggle_current_line_blame, bufnr, "Toggle line blame")
+    map('n', '<leader>gs', gitsigns.stage_hunk, bufnr, "Stage hunk under cursor")
+    map('n', '<leader>gu', gitsigns.undo_stage_hunk, bufnr, "Undo last stage hunk")
+    map('n', '<leader>gS', gitsigns.stage_buffer, bufnr, "Stage all hunks in buffer")
+    map('n', '<leader>gp', gitsigns.preview_hunk, bufnr, "Preview hunk")
 
-    vim.keymap.set('n', '<leader>gl', function() gitsigns.blame_line({ full = true }) end, { buffer = bufnr, silent = true, desc = "[GitSigns] Blame current line " })
-    vim.keymap.set('n', '<leader>gb', gitsigns.toggle_current_line_blame, { buffer = bufnr, silent = true, desc = "[GitSigns] Toggle line blame" })
-    vim.keymap.set('n', '<leader>gs', gitsigns.stage_hunk, { buffer = bufnr, silent = true, desc = "[GitSigns] Stage hunk under cursor" })
-    vim.keymap.set('n', '<leader>gu', gitsigns.undo_stage_hunk, { buffer = bufnr, silent = true, desc = "[GitSigns] Undo last stage hunk" })
-
+    map('n', ']c', gitsigns.next_hunk, bufnr, "Goto next hunk")
+    map('n', '[c', gitsigns.prev_hunk, bufnr, "Goto next hunk")
 end
 
 gitsigns.setup {
