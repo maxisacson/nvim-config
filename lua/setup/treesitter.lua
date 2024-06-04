f86dc9f713cdeec508123afc1899c539354e950d
Max Isacson <max.isacson@excillum.com>
2024-06-04 08:31:53 +0200

    treesitter: add bash parser

--- lua/setup/treesitter.lua
+++ lua/setup/treesitter.lua
@@ -1,5 +1,5 @@
 require 'nvim-treesitter.configs'.setup {
-    ensure_installed = {"lua", "vim", "vimdoc", "c", "cpp", "python"},
+    ensure_installed = { "lua", "vim", "vimdoc", "c", "cpp", "python", "bash" },
     auto_install = true,
     highlight = {
         enable = true,
