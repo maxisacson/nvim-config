bfeedef21dc6aa90a0130fec0d775965b28aa972
Max Isacson <max.isacson@excillum.com>
2024-06-04 08:25:12 +0200

    treesitter: only ensure a few parsers are installed

--- lua/setup/treesitter.lua
+++ lua/setup/treesitter.lua
@@ -1,5 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
-    ensure_installed = "all",
+    ensure_installed = {"lua", "vim", "vimdoc", "help", "c", "cpp", "python"},
+    auto_install = true,
     highlight = {
         enable = true,
     },
