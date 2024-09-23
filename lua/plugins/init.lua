3b9e9006c62c673fc9577c1db83e472dc3e63c57
Max Isacson <max.isacson@excillum.com>
2024-09-23 11:03:13 +0200

    plugins: add ~/.local to auto session suppreess dirs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -231,10 +231,10 @@ return {
     {
         'rmagatti/auto-session',
         config = function()
-            require('auto-session').setup {
+            require('auto-session').setup({
                 log_level = "error",
-                auto_session_suppress_dirs = { '~/', '/', "~/.config/*" },
-            }
+                auto_session_suppress_dirs = { '~/', '/', "~/.config/*", "~/.local/*" },
+            })
         end
     }
 }
