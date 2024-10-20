701aaba37c22159873cfdf334a29f632a520c3f7
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2024-10-20 17:43:36 +0200

    treesitter: ignore parser for tmux

--- lua/setup/treesitter.lua
+++ lua/setup/treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
     ensure_installed = { "lua", "vim", "vimdoc", "c", "cpp", "python", "bash" },
-    ignore_install =  { "dockerfile" },
+    ignore_install =  { "dockerfile", "tmux" },
     auto_install = true,
     highlight = {
         enable = true,
