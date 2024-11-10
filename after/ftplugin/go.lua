f4e0d9c6d2e8c3bd3216e1f81831b76a54ca8dcb
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2024-11-10 19:36:38 +0100

    ftplugins: better listchars for golang

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,4 +1,5 @@
 vim.bo.expandtab = false
+vim.opt_local.listchars = { tab = '  ', trail = '∙', nbsp = '␣', extends = '»', precedes = '«', lead = '∙' }
 
 local ag_autoformat = vim.api.nvim_create_augroup('AutoFormatGoFile', { clear = true })
 vim.api.nvim_create_autocmd('BufWrite', {
