d9fe30f207288d24c041d1332553cebd294d5809
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2025-05-30 10:36:18 +0200

    fix(auto-session): set sessionoptions and update config names

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -238,9 +238,10 @@ return {
     {
         'rmagatti/auto-session',
         config = function()
+            vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
             require('auto-session').setup({
                 log_level = "error",
-                auto_session_suppress_dirs = { '~/', '/', "~/.config/*", "~/.local/*" },
+                suppressed_dirs = { '~/', '/', "~/.config/*", "~/.local/*" },
             })
         end
     },
