b83d704f543f97629a6047b1d1fc00d85038494f
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2026-06-19 11:09:17 +0200

    chore: remove commented plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -160,17 +160,6 @@ return {
         config = function() require('setup.lualine') end
     },
 
-    -- delete buffers without messing up window layouts
-    -- {
-    --     'famiu/bufdelete.nvim',
-    --     config = function()
-    --         vim.keymap.set('n', '<M-q>', function() require('bufdelete').bufdelete(0, false) end,
-    --             { silent = true, desc = '[Buffer] Close buffer' })
-    --         vim.keymap.set('n', '<M-Q>', function() require('bufdelete').bufdelete(0, true) end,
-    --             { silent = true, desc = '[Buffer] Force close buffer' })
-    --     end
-    -- },
-
     -- tabline plugin
     {
         'akinsho/bufferline.nvim',
